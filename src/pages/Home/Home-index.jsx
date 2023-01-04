75a437713d59135fde4a940298596444c9566a76
Mathis Petitjean <veille.petitjean@gmail.com>
2023-01-04 22:59:48 +0100

    First commit

---
+++ src/pages/Home/Home-index.jsx
@@ -0,0 +1,21 @@
+import '../../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Kasa</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
