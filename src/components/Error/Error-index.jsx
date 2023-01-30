3121d1255b59e46debb9db1ec46566d8cdd94a49
Mathis Petitjean <veille.petitjean@gmail.com>
2023-01-30 22:46:22 +0100

    Error page

--- src/components/Error/Error-index.jsx
+++ src/components/Error/Error-index.jsx
@@ -1,9 +1,15 @@
-function Error() {
-    return (
-      <div>
-        <h1>Oups 🙈 Cette page n'existe pas</h1>
-      </div>
-    )
-  }
-  
-export default Error
+import { Link } from "react-router-dom";
+import "./Error.css";
+import "../../Utils/style-page/index.css";
+
+export default function ErrorComponent() {
+	return (
+		<div className="error_container">
+      <p className="error_code">404</p>
+			<p className="error_advertisement">Oups! La page que vous demandez n'existe pas.</p>
+			<Link to="/" className="error_link_home">
+				Retourner sur la page d’accueil
+			</Link>
+		</div>
+	);
+}
