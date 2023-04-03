fa2655d741c99f03ad07e5ee2a280af6d7a2a020
Mathis Petitjean <veille.petitjean@gmail.com>
2023-04-03 15:11:29 +0200

    Correction create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './Utils/style-page/index.css'
+import Home from './pages/Home/Home-index'
+import Accommodation from './pages/Accommodation-Sheet/Accommodation-index'
+import Header from './components/Header/Header'
+import About from './pages/About/About-index'
+import Error from './pages/Error/Error-index'
+import Footer from './components/Footer/Footer-index'
+
+function App() {
+  return (
+    <div className="global_style">
+      <div style={{ flex: 1 }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/accommodation/:id" element={<Accommodation />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './Utils/style-page/index.css'
-import Home from './pages/Home/Home-index'
-import Accommodation from './pages/Accommodation-Sheet/Accommodation-index'
-import Header from './components/Header/Header'
-import About from './pages/About/About-index'
-import Error from './pages/Error/Error-index'
-import Footer from './components/Footer/Footer-index'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Router>
-      <div className="global_style">
-        <div style={{ flex: 1 }}>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/accommodation/:id" element={<Accommodation />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
